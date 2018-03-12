88d0018c7be3d78bcfd0e3c66f0953840a7eb6b7
Reece Turner <LoJac10@users.noreply.github.com>
2018-03-11 21:35:06 -0400

    Update util.c

--- util.c
+++ util.c
@@ -1,1 +1,16 @@
+// util.c -- utility function file
 
+//------------------------------------------------------------------------------------------	FUNCTION: CLEAR ENCODERS
+void
+clearEncodersQuad_func(){
+	// Command for 393 Quad Encoder to Reset Encoder Counts to Zero
+	SensorValue[quadL] = 0;
+	SensorValue[quadR] = 0;
+}
+
+void
+clearEncodersIME_func(){
+// Command for 393 Integrated Motor Encoder to Reset Encoder Counts to Zero
+	SensorValue[imeL] = 0;  				
+	SensorValue[imeR] = 0;				  
+}
